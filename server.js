8e65898f2d0a918dfaa64dfe53c71e742b327cdb
hdminh <minhcine99@gmail.com>
2020-07-28 09:30:02 +0700

    add GET route

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express =require('express');
 const app = express();
 
+const mockUserData=[
+    {name: 'Mark'},
+    {name: 'Jill'}
+]
+app.get('/users', (req, res)=>{
+    res.json({
+        success: true,
+        message: 'noce try, bro!',
+        users: mockUserData
+    })
+})
+
+
+
 app.listen(8000, () =>{
     console.log("Server is running at 3000");
 })
-
