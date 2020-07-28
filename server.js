774f737d5c591b69ec76730c5ae2073ea0dd740c
hdminh <minhcine99@gmail.com>
2020-07-28 09:33:54 +0700

    add dynamic GET route

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ app.get('/users', (req, res)=>{
     })
 })
 
-
+app.get('/users/:id', (req, res) => {
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'i got u bro!',
+        user: req.params.id
+    })
+})
 
 app.listen(8000, () =>{
     console.log("Server is running at 3000");
